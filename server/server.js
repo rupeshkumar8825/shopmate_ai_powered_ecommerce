ae577bc40d51a6709561bf2fcc7409b450efcbf2
Rupesh Kumar <therupeshkumar8825@gmail.com>
2026-01-14 20:17:26 +0530

    Remove the server and create the backend project using the tsc as proejct type

--- server/server.js
+++
@@ -1,8 +0,0 @@
-import app from "./app.js"
-
-// create the server here with all the configuration done 
-// in the app.js
-
-app.listen(process.env.PORT, () => {
-    console.log(`Server is running at port ${process.env.PORT}`);
-})
