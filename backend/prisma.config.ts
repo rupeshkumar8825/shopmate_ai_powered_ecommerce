329f9060e477d2f4d46d9e81e116a7a08d6a7ede
Rupesh Kumar <therupeshkumar8825@gmail.com>
2026-01-14 22:11:32 +0530

    succesfully setup the prisma and then create the first migration of the applicatio

--- backend/prisma.config.ts
+++ backend/prisma.config.ts
@@ -7,11 +7,4 @@ import { ENV } from "./src/config/env";
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
-  migrations: {
-    path: "prisma/migrations",
-  },
-  engine: "classic",
-  datasource: {
-    url: env("DATABASE_URL"),
-  },
 });
