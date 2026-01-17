36bfa91f3ee8280cc989f510a1539b2276907922
Rupesh Kumar <therupeshkumar8825@gmail.com>
2026-01-17 10:56:27 +0530

    Delete backend and shift it to the learnings repo. And create a fresh repo for server of the shopmate backend

--- backend/src/errors/errorCodes.ts
+++
@@ -1,7 +0,0 @@
-// this defines the different codes for the application 
-export const ErrorCodes =  {
-    BAD_REQUEST : 400, 
-    NOT_FOUND : 404, 
-    CONFLICT : 409, 
-    INTERNAL_ERROR : 500, 
-} as const;
