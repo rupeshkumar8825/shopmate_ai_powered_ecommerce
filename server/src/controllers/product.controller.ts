1dfad208483dd60d326a197e6a485ce0b6ee0f6e
Rupesh Kumar <therupeshkumar8825@gmail.com>
2026-04-25 21:30:29 +0530

    Start implementation of product related apis. Basic templates for the product related apis done. Start createProduct api implementation by modifying controller, service and so on

--- server/src/controllers/product.controller.ts
+++ server/src/controllers/product.controller.ts
@@ -1,17 +1,40 @@
 // this will have all the controllers which are required for handling all the product 
 // related things
 
+import { Request, Response } from "express";
+
 export class ProductController {
-    static async getProduct() {
-
-    }
-
-
-    static async deleteProduct() {
-
-    }
-
-    static async getListOfAllProductsGivenUser() {
+    // controller function to create the product
+    static async createProductController (request : Request, response : Response) {
+        // consider that the data from user is already validated using zod
+        // lets start using the data as it is
+        const productName : string = request.body.name;
+        const description : string = request.body.description; 
+        // note that the using parseFloat may not be ideal. Either its better to use 
+        // zod validation or use a stricter float parsing thing
+        // but for now lets use this function and store it in the number datatype as
+        // numbers can store the integer as well as the float values.
+        const price : number = parseFloat(request.body.price);
+        const category : string = request.body.category; 
+        const images = request.files;
+        const stock : number = parseInt(request.body.stock)
         
+        // lets pass all these to the service layer
+
+
+    }
+
+
+    static async getProductController() {
+
+    }
+
+
+    static async deleteProductController() {
+
+    }
+
+    static async getListOfAllProductsGivenUserController() {
+
     }
 }
--- server/src/services/product.service.ts
+++ server/src/services/product.service.ts
@@ -1,6 +1,53 @@
 // file having all the service functions related to the product 
 
+import fileUpload from "express-fileupload";
+import prisma from "../config/prisma";
+import AppError from "../middlware/errorHandler";
+import { StatusCodes } from "../error/statusCodes";
+import { Product } from "../generated/prisma/client";
+
 export class ProductService {
+
+
+    // service function to create the product and add it into the database
+    static async createProductService (productName : string, description : string, price : number, category : string, 
+        images : fileUpload.FileArray | null | undefined, stock : number
+    ) {
+
+        // basic validations and checks comes here 
+
+        if(!productName || !description || !price || !stock || !category
+            || (productName.trim().length == 0) || (description.trim().length == 0) 
+            || (category.trim().length == 0) || (price < 0) || (stock < 0))
+        {
+            // the input values are not valid hence lets throw an error 
+            throw new AppError("One or more input fields are invalid", StatusCodes.BAD_REQUEST_400)   
+        }
+
+        productName = productName.trim();
+        description = description.trim();
+        category = category.trim();
+
+        // upload the product images on the cloudinary 
+
+        // update the database
+        const createdProduct : Product|null = null;
+        // const createdProduct = await prisma.product.create({
+        //     data : {
+        //         name : productName, 
+        //         description : description, 
+        //         price : price, 
+        //         category : category,
+        //         ratings : 0, 
+
+        //     }
+        // })
+
+        // return the response to the controller layer 
+        // say everything went fine 
+        return createdProduct;
+    }
+
     static async getProductService () {
 
     }
