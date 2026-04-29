187dea5d8634d40b147ae312ea531033f292892f
Rupesh Kumar <therupeshkumar8825@gmail.com>
2026-04-29 22:00:18 +0530

    Add product routes for delete product

--- server/src/routes/product.routes.ts
+++ server/src/routes/product.routes.ts
@@ -6,9 +6,10 @@ import { validateRole } from "../middlware/validate.role.middleware";
 const productRoutes = Router();
 
 // lets define the outline of all the possible routes
-productRoutes.post("/admin/create", authMiddleware, validateRole("Admin"), ProductController.createProductController)
+productRoutes.post("/admin/create", authMiddleware, validateRole("Admin"), ProductController.createProductController);
 productRoutes.get("/", ProductController.fetchAllProductsController) // note that we do not need to pass the auth middleware here
-productRoutes.put("/:productId",authMiddleware, validateRole("Admin"),  ProductController.updateProductController)
+productRoutes.put("/:productId",authMiddleware, validateRole("Admin"),  ProductController.updateProductController);
+productRoutes.delete("/:productId", authMiddleware, validateRole("Admin"), ProductController.deleteProductController);
 
 // say everything went fine 
 export default productRoutes
