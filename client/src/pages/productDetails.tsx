c4276dd367f54670cc47d6cb771c5548b8d32074
Rupesh Kumar <therupeshkumar8825@gmail.com>
2026-05-10 13:01:57 +0530

    Add more page for the ecommerce application

---
+++ client/src/pages/productDetails.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+export const ProductDetailsPage = () => {
+    return (
+        <>
+            <div>
+                Welcome to the product details page
+            </div>
+        </>
+    )
+}
---
+++ client/src/pages/products.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const ProductPage = () => {
+    return (
+        <>
+            <div>
+                Welcome to the product page for this purpose
+            </div>
+        </>
+    )
+}
