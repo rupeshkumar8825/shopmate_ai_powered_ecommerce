22d4f5d295617dcfc01e4a506ceed3783446457a
Rupesh Kumar <therupeshkumar8825@gmail.com>
2026-05-27 22:45:07 +0530

    Completing the navbar with backend

--- client/src/components/Layout/NavbarLayout.tsx
+++ client/src/components/Layout/NavbarLayout.tsx
@@ -1,9 +1,44 @@
 // this is the navbar component of the website 
 
+import { Menu, Search, ShoppingCart, User } from "lucide-react"
+import { useState } from "react";
+
 export const NavbarLayout = () => {
+
+    // all recoil related states of this component comes here 
+
+    // states related to this component comes here 
+    const [isSideBarOpen, setIsSideBarOpen] = useState<boolean>(false);
+    const [isSearchBarOpen, setIsSearchBarOpen] = useState<boolean>(false);
+    const [isUserAuthOpen, setIsUserAuthOpen] = useState<boolean>(false)
+
+    // all the handlers of the component comes here 
+    const sideBarClickHandler = () => {
+        setIsSideBarOpen(!isSideBarOpen);
+    }
+    
+    const searchBarClickHandler = () => {
+        setIsSearchBarOpen(!isSearchBarOpen)
+    }
+
+    const userAuthProfileClickHandler = () => {
+        setIsUserAuthOpen(!isUserAuthOpen)
+    }
+    
+    // all hooks related to this component comes here 
+
+
+
+
     return (
-        <div className="flex flex-row justify-around items-center py-5 shadow-2xl bg-neutral-200 rounded-lg">
+        <div className="relative flex flex-row justify-around items-center py-5 shadow-2xl bg-neutral-200 rounded-lg">
+            {/* side bar of the navbar comes here  */}
+            <Menu onClick={sideBarClickHandler}></Menu>
+
+            {/* app name comes here i.e. Shopmate text comes here */}
             <h1 className="font-semibold text-2xl">ShopMate</h1>
+
+            {/* actual navbar comes here */}
             <nav>
                 <ul className="flex flex-row gap-10 justify-around items-center">
                     <li>Home</li>
@@ -13,6 +48,30 @@ export const NavbarLayout = () => {
                     <li>Contact</li>
                 </ul>
             </nav>
+
+            {/* search overlay comes here  */}
+            <button onClick={searchBarClickHandler}>
+             <Search></Search>
+            </button>
+
+            {/* user profile logo comes here */}
+            <button onClick={userAuthProfileClickHandler}>
+                <User></User>
+            </button>
+
+            {/* cart comes here  */}
+            <button className="relative">
+                <ShoppingCart></ShoppingCart>
+                <span className="absolute -top-1 -right-1 bg-red-500 text-white text-xs rounded-full w-3 h-3 flex justify-center items-center p-2">5</span>
+            </button>
+
+            {/* depending on whether the side bar is opened by the user or not 
+            we will show the below side bar on the half of the screen for this purpose  */}
+            {
+                isSideBarOpen ? <div className="absolute border-2 border-red-500 ">
+
+                </div> : null
+            }
         </div>
     ) 
 }
