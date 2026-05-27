a4e4d2c85298d54f751a980cfb4521f24c510355
Rupesh Kumar <therupeshkumar8825@gmail.com>
2026-05-27 18:50:46 +0530

    Add the auth related interfaces to be used in the frontend. Note that these types should be consistent with the backend implementation

--- client/src/api/authApi.ts
+++ client/src/api/authApi.ts
@@ -1,5 +1,15 @@
 // all the apis related to user authentication comes here
 
-export const registerUser = async () => {
-    
+import type { RegisterPayload } from "../types/auth.types"
+import axiosInstance from "./axiosInstance"
+
+
+
+export const registerUser = async (payload : RegisterPayload) : Promise<boolean> => {
+    // given the payload and the axiosinstance we need to 
+    // make the post api call to register the user for this purpose 
+    const registerUserResponse = await axiosInstance.post("/v1/auth/register", )
+
+    // say everything went fine 
+    return registerUserResponse
 }
--- client/src/types/auth.types.ts
+++ client/src/types/auth.types.ts
@@ -1,23 +1,62 @@
 // all the types related to the authentication of the user comes here
 
 // defining teh register payload to be sent to the backend server 
-export type RegisterPayload = {
+export  interface RegisterPayload  {
     name : string, 
     email : string, 
     password : string
 }
 
 
-// type for defining the payload to be sent to the backend server 
+//TODO : Check how to define the roles as an enum in the ts and 
+// then try to use this enum type for the role field in case of 
+// user type for this purpose
+// enum Role {
+//     User = 0, 
+//     Admin = 1
+// }
+
+export interface User {
+    name: string;
+    id: string;
+    email: string;
+    password: string;
+    role: "User" | "Admin";
+    avatar: JSON | null;
+    resetPasswordToken: string | null;
+    resetPasswordExpires: Date | null;
+    createdAt: Date
+}
+
+
+export interface RegisterResponse {
+    success : string, 
+    user : User,
+    message : string, 
+}
+
+
+//  interface for defining the payload to be sent to the backend server 
 // for logging into the application
-export type LoginPayload = {
+export  interface LoginPayload  {
     email : string, 
     password : string
 }
 
+export interface LoginResponse {
+    success : boolean, 
+    message : string, 
+    token : string
+}
 
-// type for defining the payload to be sent to the backend server
+//  interface for defining the payload to be sent to the backend server
 // for logging out of the application 
-export type LogPayload = {
+export  interface LogPayload  {
 
+}
+
+
+export interface APIFailPayload {
+    success : string, 
+    message : string
 }
