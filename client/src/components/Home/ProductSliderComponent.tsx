c8021e0203db1d47ec7107ca2974215831af3ff5
Rupesh Kumar <therupeshkumar8825@gmail.com>
2026-06-06 20:02:43 +0530

    Product slider is complete. Add the product slider component to show all products list and top rated products too

--- client/src/components/Home/ProductSliderComponent.tsx
+++ client/src/components/Home/ProductSliderComponent.tsx
@@ -1,16 +1,20 @@
 // given some list of products this component is going to show the product slider 
 
 import { Link } from "react-router"
-import { Link } from "react-router"
 import type { ProductSliderComponentProps } from "../../types/homePageTypes"
 import type { ProductDetail } from "../../types/product.types"
+import { useEffect } from "react"
 
 export const ProductSliderComponent = (props : ProductSliderComponentProps) => {
+
+    useEffect(() => {
+        console.log("list of products in the product slider component", props.listOfProducts)
+    }, [])
+
     return (
         <div className="flex flex-col justify-center items-center bg-background-500 p-20 gap-5">
                     {/* here comes the heading of the application for this purpose */}
                     <h1 className="text-white text-4xl text-center font-bold">Shop by Category</h1>
-                    <h1 className="text-neutral-300 text-xl font-light">Discover our wide range of products across different categories</h1>
         
         
                     {/* here comes the category related cards for this purpose */}
@@ -66,8 +70,24 @@ export const ProductSliderComponent = (props : ProductSliderComponentProps) => {
                                                 }
                                             </div> 
 
+                                            {/* quick add to cart icon button comes here*/}
+                                            <button disabled={currProduct.stock === 0} className="bg-blue-600 hover:bg-blue-700 text-white px-4 py-2 rounded-md mt-2">
+                                                Add to Cart
+                                            </button>
+                                            
+                                            {/* product ratings and also show the total review count  comes here */}
+                                            <div className="flex flex-row items-center gap-1">
+                                                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-yellow-400" viewBox="0 0 20 20" fill="currentColor">
+                                                    <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.286 3.966a1 1 0 00.95.69h4.178c.969 0 1.371 1.24.588 1.81l-3.385 2.46a1 1 0 00-.364 1.118l1.286 3.966c.3.921-.755 1.688-1.538 1.118l-3.385-2.46a1 1 0 00-1.175 0l-3.385 2.46c-.783.57-1.838-.197-1.538-1.118l1.286-3.966a1 1 0 00-.364-1.118L2.049 9.393c-.783-.57-.38-1.81.588-1.81h4.178a1 1 0 00.95-.69l1.286-3.966z" />
+                                                </svg>
+                                                <span className="text-white text-sm">{currProduct.ratings}</span>
+                                                <span className="text-white text-sm">({currProduct.reviewList.length})</span>
+                                            </div>
 
-
+                                            {/* product price comes here */}
+                                            <div className="mt-2">
+                                                <span className="text-white text-lg font-bold">${currProduct.price.toFixed(2)}</span>
+                                            </div>
 
                                         </div>
                                     </Link>
--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -4,14 +4,37 @@ import { CategorySectionComponent } from "../components/Home/CategorySectionComp
 import { FeatureSectionComponent } from "../components/Home/FeatureSectionComponent";
 import { NewsLetterSectionComponent } from "../components/Home/NewsLetterSectionComponent";
 import { NavbarLayout } from "../components/Layout/NavbarLayout";
+import { ProductSliderComponent } from "../components/Home/ProductSliderComponent";
+import { useProduct } from "../hooks/useProduct";
 
 export const HomePage = () => {
+
+    // lets get the list of all products from the product hook 
+    const { allProductList, topRatedProducts } = useProduct();
+
     return(
         <div className="">
             {/* Welcome to the home page. */}
             <HeroSliderSectionComponent autoPlay={true} autoPlayDuration={3000} pauseOnHover={true}></HeroSliderSectionComponent>
             <CategorySectionComponent></CategorySectionComponent>
             <FeatureSectionComponent></FeatureSectionComponent>
+            {/* we will show the allProducts and topratedproducts product slider too */}
+            <div className="w-full h-px bg-neutral-700 my-10">
+                {
+                    allProductList.length > 0 && (
+                        <div className="flex flex-col gap-10">
+                            <ProductSliderComponent listOfProducts={allProductList}></ProductSliderComponent>
+                        </div>
+                    )
+                }
+                {
+                    topRatedProducts.length > 0 && (
+                        <div className="flex flex-col gap-10">
+                            <ProductSliderComponent listOfProducts={topRatedProducts}></ProductSliderComponent>
+                        </div>
+                    )
+                }
+            </div>
             <NewsLetterSectionComponent></NewsLetterSectionComponent>
         </div>
     )
