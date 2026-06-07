4da1e9b882fbc892320df65879b6dd991f359622
Rupesh Kumar <therupeshkumar8825@gmail.com>
2026-06-07 15:55:28 +0530

    add to cart functionality now working as expected. Able to add and remove the items

--- client/src/hooks/useCart.ts
+++ client/src/hooks/useCart.ts
@@ -9,31 +9,41 @@ export const useCart = () => {
     const [cartItems, setCartItems] = useRecoilState(cartItemsAtom);
 
     const addToCart = (item : CartItem) => {
-        // check if the item is already in the cart 
-        const existingItemIndex = cartItems.findIndex(cartItem => cartItem.id === item.id);
-        if(existingItemIndex !== -1) {
-            // if the item is already in the cart then we need to update the quantity of that item
-            const updatedCartItems = [...cartItems];
-            updatedCartItems[existingItemIndex].quantity += item.quantity;
-            setCartItems(updatedCartItems);
-        }else {
-            // if the item is not in the cart then we need to add that item to the cart 
-            setCartItems([...cartItems, item]);
-        }
+        setCartItems((prev) => {
+            // check if the item is already in the cart 
+            const existingItemIndex = prev.findIndex(cartItem => cartItem.id === item.id);
+            if(existingItemIndex !== -1) {
+                // if the item is already in the cart then we need to update the quantity of that item
+                const updatedCartItems = [...prev];
+                updatedCartItems[existingItemIndex] = {
+                    ...updatedCartItems[existingItemIndex],
+                    quantity: updatedCartItems[existingItemIndex].quantity + item.quantity
+                };
+                return updatedCartItems;
+            }else {
+                // if the item is not in the cart then we need to add that item to the cart 
+                return [...prev, item];
+            }
+        });
     }
 
     const removeFromCart = (itemId : string) => {
-        const updatedCartItems = cartItems.filter(cartItem => cartItem.id !== itemId);
-        setCartItems(updatedCartItems);
+        setCartItems((prev) => prev.filter(cartItem => cartItem.id !== itemId));
     }
 
     const updateCartItemQuantity = (itemId : string, quantity : number) => {
-        const existingItemIndex = cartItems.findIndex(cartItem => cartItem.id === itemId);
-        if(existingItemIndex !== -1) {
-            const updatedCartItems = [...cartItems];
-            updatedCartItems[existingItemIndex].quantity = quantity;
-            setCartItems(updatedCartItems);
-        }
+        setCartItems((prev) => {
+            const existingItemIndex = prev.findIndex(cartItem => cartItem.id === itemId);
+            if(existingItemIndex !== -1) {
+                const updatedCartItems = [...prev];
+                updatedCartItems[existingItemIndex] = {
+                    ...updatedCartItems[existingItemIndex],
+                    quantity
+                };
+                return updatedCartItems;
+            }
+            return prev;
+        });
     }
 
     const clearCart = () => {
