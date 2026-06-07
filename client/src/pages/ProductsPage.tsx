f7732779c3956e848c3fca8c78364105c5fbd9f6
Rupesh Kumar <therupeshkumar8825@gmail.com>
2026-06-07 18:53:25 +0530

    Built the UI for the normal product search bar and using AI search bar

--- client/src/pages/ProductsPage.tsx
+++ client/src/pages/ProductsPage.tsx
@@ -4,6 +4,7 @@ import { useProduct } from "../hooks/useProduct"
 import { ProductSectionComponent } from "../components/Products/ProductSectionComponent"
 import { PaginationComponent } from "../components/Products/PaginationComponent"
 import { FilterComponent } from "../components/Products/FilterSectionComponent"
+import { Sparkle, Sparkles } from "lucide-react"
 
 const PRODUCTS_PER_PAGE = 6
 
@@ -30,6 +31,8 @@ export const ProductsPage = () => {
                     </p>
                 </div>
 
+                
+
                 {/* Main layout — filter sidebar + product section */}
                 <div className="flex flex-row gap-8 items-start">
 
@@ -37,10 +40,21 @@ export const ProductsPage = () => {
                     <FilterComponent
                         searchFilter={searchFilter}
                         updateFilters={updateFilters}
-                    />
+                        />
 
                     {/* Right — Products + Pagination */}
                     <div className="flex flex-col gap-6 flex-1 min-w-0">
+                        {/* here comes the Search query where we can search the product with its name */}
+                        <div className="flex flex-row justify-between items-center gap-5 w-full">
+                            {/* here comes the actual search bar */}
+                            <input type="search" className="p-3 bg-component-background-500 text-white w-[80%] rounded-lg" placeholder="Search Products" />
+                            {/* AI search button comes here  */}
+                            <button className="px-4 py-2 rounded-lg bg-component-background-500 text-white font-semibold border-2 border-pink-400 flex justify-center items-center gap-2">
+                                <Sparkles className="w-4 h-4"></Sparkles>    
+                                <p>AI Search</p>
+                            </button>
+                        </div>
+
 
                         {/* Product grid */}
                         <ProductSectionComponent
