574b7fc8599e5a7b4d4e20de7378f24db51188a4
Rupesh Kumar <therupeshkumar8825@gmail.com>
2026-06-08 23:10:49 +0530

    Start implementation of the cart page of the application. Set the flow and skeleton for the same in the code

--- client/src/pages/CartPage.tsx
+++ client/src/pages/CartPage.tsx
@@ -1,4 +1,33 @@
+import { useNavigate } from "react-router-dom";
+import { useCart } from "../hooks/useCart"
+
+
+// COMPONENT CONSTANTS COMES HERE 
+const SHIPPING_THRESHOLD = 50;
+const SHIPPING_COST = 2;
+const TAX_RATE = 0.18;
+
 export const CartPage = () => {
+    // all the states related to this component comes here 
+    
+    
+    // all the hook functions to be used from different custom hooks comes here
+    const { cartItems, updateCartItemQuantity, removeFromCart, addToCart } = useCart(); 
+    
+    // all the inbuilt hooks of the component comes here 
+    const navigate = useNavigate();
+    
+
+    // handle the case of empty cart for this case 
+    if(!cartItems || cartItems.length === 0) {
+        // this means that the cart is empty 
+        return (
+            <div>
+                Cart it empty. Continue for shopping for this purpose
+            </div>
+        )
+    }
+
     return (
         <div>
             Welcome to the cart page on the ecommerce website
